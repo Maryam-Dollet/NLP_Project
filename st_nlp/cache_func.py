75cd82663fe9dd1a55e7655e5a3782fdb859e3bf
Maryam-Dollet <maryam.dollet@gmail.com>
2024-01-20 16:12:16 +0100

    update st

--- st_nlp/cache_func.py
+++ st_nlp/cache_func.py
@@ -1,1 +1,18 @@
 import streamlit as st
+import pandas as pd
+import pickle
+
+
+@st.cache_data
+def load_companies_translated():
+    return pd.read_csv("data/company_desc_translated.csv", sep=";").dropna(ignore_index=True)
+
+
+@st.cache_data
+def load_reviews_sample():
+    return pd.read_csv("data/reviews_sample_translated.csv", sep=";").dropna(ignore_index=True)
+
+
+@st.cache_data
+def load_category():
+    return pd.read_csv("data/category_data.csv", sep=";").dropna(ignore_index=True)
--- st_nlp/pages/1_Web_Scraping.py
+++ st_nlp/pages/1_Web_Scraping.py
@@ -1,3 +1,24 @@
 import streamlit as st
+import pandas as pd
+from cache_func import load_companies_translated, load_reviews_sample, load_category
+
+st.set_page_config(layout="wide")
+
+df = pd.concat([load_companies_translated(), load_category()], axis=1)
+df2 = load_reviews_sample()
 
 st.title("Data Gathering: Web Scraping french companies on Trustpilot")
+
+st.write("files: scrap_trust_pilot.ipynb and scrap_trust_pilot_2.ipynb")
+
+st.write("Using Beautiful Soup to scrap the website first we extracted the links to the categories listed in this link:")
+
+st.write("https://fr.trustpilot.com/categories")
+
+st.markdown("#### Here are the categories extracted")
+
+st.dataframe(df["category"].unique(), use_container_width=True)
+
+st.markdown("#### Sample of the reviews we extracted")
+
+st.dataframe(df2)
