902812fa387f48fbcf0a623cc5e3d9c9c2e090e0
Maryam-Dollet <maryam.dollet@gmail.com>
2024-01-20 20:36:38 +0100

    update

--- st_nlp/pages/4_Embedding.py
+++ st_nlp/pages/4_Embedding.py
@@ -112,11 +112,13 @@ fig_3d = px.scatter_3d(
 )
 fig_3d.update_layout(width=1300 ,height=1000)
 fig_3d.update_traces(marker_size=2)
+
 st.plotly_chart(fig_3d)
 
 st.subheader("Using UMAP and HDBScan for Clustering")
 
 hdbscan_df1 = hdbscan_cluster(result5)
+hdbscan_df1['category'] = hdbscan_df1['category'].replace('-1' ,'outlier')
 # st.dataframe(hdbscan_df1)
 st.write(f"Number of Ouliers Detected: {len(hdbscan_df1[hdbscan_df1['category'] == '-1'])}")
 
@@ -125,17 +127,21 @@ fig_3d = px.scatter_3d(
 )
 fig_3d.update_layout(width=1300 ,height=1000)
 fig_3d.update_traces(marker_size=3)
+fig_3d.update_traces(visible="legendonly", selector=lambda t: not t.name in hdbscan_df1["category"].unique()[1:])
 st.plotly_chart(fig_3d)
 
 hdbscan_df2 = hdbscan_cluster(result6)
 # st.dataframe(hdbscan_df2)
+hdbscan_df2['category'] = hdbscan_df2['category'].replace('-1' ,'outlier')
 st.write(f"Number of Ouliers Detected: {len(hdbscan_df2[hdbscan_df2['category'] == '-1'])}")
+# st.write(hdbscan_df2["category"].unique())
 
 fig_3d = px.scatter_3d(
     hdbscan_df2, x="x", y="y", z="z", hover_name="word", color="category"
 )
 fig_3d.update_layout(width=1300 ,height=1000)
 fig_3d.update_traces(marker_size=3)
+fig_3d.update_traces(visible="legendonly", selector=lambda t: not t.name in hdbscan_df2["category"].unique()[1:])
 st.plotly_chart(fig_3d)
 
 st.subheader("Tensorboard")
